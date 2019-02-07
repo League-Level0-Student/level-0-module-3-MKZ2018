fc729ee7145625145558c8047adde0b61957a4ed
League <league@WTS9.attlocal.net>
2019-02-06 19:57:37 -0800

    module 3 day one

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,16 +5,24 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+		String quantity = JOptionPane.showInputDialog("How many cats do you have?");
+		
 		// 2. Convert their answer into an int
-
+int cats = Integer.parseInt(quantity);
+if(cats = 3) {
+JOptionPane.showMessageDialog(null, "crazy cat person!");
+}
+}
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+if(cats = 0) {
+	JOptionPane.showMessageDialog(null, "");
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+	
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
 		
 	}
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,46 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		 
+
 		//3. Ask the user what color they would like the robot to draw
 		
+		for (int i = 0; i < args.length; i++) {
+			Robot bobby;
+		
+		String color;
+		color = JOptionPane.showInputDialog("what color? Red, Green, or Blue?");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+if(color.equals("Blue")) {
+bobby.setPenColor(0, 0, 100);
+}
+else(color.equals("Red")); {
+	bobby.setPenColor(100,0,0);
+}
+else(color.equals("Green")){
+	bobby.setPenColor(0,100,0);
+}
         //6. If the user doesn’t enter anything, choose a random color
-
+if(color.equals("Red")) {
+	bobby.setRandomPenColor();
+}
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
-		
+		bobby.setPenWidth(10);
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+bobby.penDown();
+		bobby.move(20);
+		bobby.move(10);
+		}
+		
 	}
 }
