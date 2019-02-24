9df08f667d930eac5d4b4095fad086b6e9ea3a59
Contreras123456 <league@WorthyMini-6.attlocal.net>
2019-02-24 11:27:31 -0800

    module 3 modulo

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -10,15 +10,24 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
+	
 		Robot bobby = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		bobby.setX(50);
-		bobby.setY(100);
+		bobby.setY(250);
+		bobby.setSpeed(30);
+		bobby.penDown();
+		bobby.setRandomPenColor();
+		bobby.turn(90);
 		// You also need to show the robot to see the result of this line.
-for (int i = 0; i < 5; i++) {
-		bobby.move(144);
-		bobby.turn(45);
+
+		
+		for (int i = 0; i < 5; i++) {
+		bobby.move(30);
+		bobby.turn(145);
 }
+
+			
 		// 2. Make the robot draw a star shape. Hint: 144.
 
 		// 3. Set the size of the star to 30.
@@ -26,7 +35,7 @@ for (int i = 0; i < 5; i++) {
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/RobotWalk
+		 http://bit.ly/RobotWalk
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
---
+++ src/modulo/bullseye/bullseye.pde
@@ -0,0 +1,17 @@
+void setup() {
+  size(500,500);
+}
+
+void draw() {  
+  fill(#FF0303);
+  ellipse(150,150,200,200); 
+  fill(#050000);
+  ellipse(150,150,160,160);
+  fill(#FF0303);
+  ellipse(150,150,120,120);
+  fill(#050000);
+  ellipse(150,150,80,80);
+  fill(#FF0303);
+  ellipse(150,150,40,40);
+  
+  }
